d8e0c14978ab4719a516a44d5d5e33db264f4b2b
endedech <97050033+endedech@users.noreply.github.com>
2024-04-09 22:20:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const { CommandClient } = require('eris')
 
-// Stupid ass bot creation
+// tworzenie zjebanego bota
 async function init(token) {
     const stupidAssBot = new CommandClient(`Bot ${token}`, { intents: ['guilds'], maxShards: 'auto',restMode: true })
-    // Register the stupid ass command
+    // zarejestruj zjebana komende
     stupidAssBot.on('ready', async () => {
         await stupidAssBot.bulkEditCommands([{
             name: 'lol',
-            description: 'I hate discord so much you cannot believe it',
+            description: 'nienawidze diskorda pozdro',
             type: 1,
         }])
-        console.log(`Paste the URL below into your browser to invite your bot!\nhttps://discord.com/oauth2/authorize?client_id=${stupidAssBot.user.id}&scope=applications.commands%20bot&permissions=3072`)
+        console.log(`zaproszenie do bota\nhttps://discord.com/oauth2/authorize?client_id=${stupidAssBot.user.id}&scope=applications.commands%20bot&permissions=3072`)
     })
     // Stupid ass interaction creation event
     stupidAssBot.on('interactionCreate', async (interaction) => {
         if (interaction?.data?.name === 'lol') {
             await interaction.createMessage({
-                content: 'According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway, because bees don\'t care what humans think is impossible.'
+                content: 'Litwo, Ojczyzno moja ty jesteś jak zdrowie Ile cię trzeba cenić, ten tylko się dowie Kto cię stracił Dziś piękność twą w całej ozdobieWidzę i opisuję bo tęsknię po tobie.'
             })
-            console.log('Self destructing...')
+            console.log('autodestrukcja...')
             process.exit(0)
         }
     })
